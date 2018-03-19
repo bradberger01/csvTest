8215d769834b75f741151d90a050ac60666e7b08
bradberger01 <bradberger@hotmail.com>
2018-03-19 18:16:40 -0400

    Split line into line items

---
+++ CsvTest/LineItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTest
+{
+    class LineItem
+    {
+        public string Guid { get; set; }
+        public string Val1 { get; set; }
+        public string Val2 { get; set; }
+        public string Val3 { get; set; }
+    }
+}
--- CsvTest/Program.cs
+++ CsvTest/Program.cs
@@ -15,11 +15,36 @@ namespace CsvTest
 
             List<string> lines = File.ReadAllLines(filePath).ToList();
 
+            int numberOfLines = 0;
+
             foreach (string line in lines)
             {
-                Console.WriteLine(line);
+                numberOfLines++;
+            }
+            numberOfLines = numberOfLines - 1;
+            //Because top line are names of items not records
+            Console.WriteLine("Number of records in the file: " + numberOfLines);
+
+            List<LineItem> items = new List<LineItem>();
+
+            foreach (string line in lines)
+            {
+                string[] entries = line.Split(',');
+
+                LineItem newLineItem = new LineItem();
+
+                newLineItem.Guid = entries[0];
+                newLineItem.Val1 = entries[1];
+                newLineItem.Val2 = entries[2];
+                newLineItem.Val3 = entries[3];
+
+                items.Add(newLineItem);
             }
 
+            foreach (var lineItem in items)
+            {
+                Console.WriteLine($"{lineItem.Guid}");
+            }
         }
     }
 }
