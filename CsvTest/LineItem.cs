4a2055744358ada9276305cf2dbc6decfbbe958e
bradberger01 <bradberger@hotmail.com>
2018-03-21 11:45:17 -0400

    converted Val1 and Val2 to ints

--- CsvTest/LineItem.cs
+++ CsvTest/LineItem.cs
@@ -9,8 +9,30 @@ namespace CsvTest
     class LineItem
     {
         public string Guid { get; set; }
-        public string Val1 { get; set; }
-        public string Val2 { get; set; }
+        public int Val1 { get; set; }
+        public int Val2 { get; set; }
         public string Val3 { get; set; }
+        public int numVal1 { get; set; }
+        public int numVal2 { get; set; }
+
+        //static LineItem FromTheLine(string line)
+        //{
+        //    var data = line.Split(',');
+
+        //    return new LineItem()
+        //    {
+        //        Guid = data[0],
+        //        Val1 = int.Parse(data[1]),
+        //        Val2 = int.Parse(data[2]),
+        //        Val3 = data[3],
+        //    };
+        //}
+
+        //static int GetLargestSumOfVals()
+
+       
+        
     }
+
+   
 }
--- CsvTest/Program.cs
+++ CsvTest/Program.cs
@@ -10,11 +10,11 @@ namespace CsvTest
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //reading the file, may need to cahnage directory of file depending on where it lives
             string filePath = @"C:\Users\WeCanCodeIT\Desktop\test.csv";
 
             List<string> lines = File.ReadAllLines(filePath).ToList();
-
+            //Count the lines
             int numberOfLines = 0;
 
             foreach (string line in lines)
@@ -25,25 +25,32 @@ namespace CsvTest
             //Because top line are names of items not records
             Console.WriteLine("Number of records in the file: " + numberOfLines);
 
+            //Split lines into line items GUID, Val1, Val2, Val3
             List<LineItem> items = new List<LineItem>();
 
             foreach (string line in lines)
             {
-                string[] entries = line.Split(',');
+                string[] entries = line.Replace('"', ' ').Trim().Split(',');
+                int number;
+                int number2;
+                bool res = int.TryParse(entries[1], out number);
+                bool res2 = int.TryParse(entries[2], out number2);
+
 
                 LineItem newLineItem = new LineItem();
 
                 newLineItem.Guid = entries[0];
-                newLineItem.Val1 = entries[1];
-                newLineItem.Val2 = entries[2];
+                newLineItem.Val1 = number;
+                newLineItem.Val2 = number2;
                 newLineItem.Val3 = entries[3];
 
                 items.Add(newLineItem);
+                
             }
 
             foreach (var lineItem in items)
             {
-                Console.WriteLine($"{lineItem.Guid}");
+                Console.WriteLine(lineItem.Val1 + lineItem.Val2);
             }
         }
     }
